e2767404aacd54b13f314bd8ad79903912f898fb
App Academy Student <student@appacademy.io>
2017-09-12 16:12:56 -0700

    refactor todolist

--- day2/TodoApp/app/assets/javascripts/bundle.js
+++ day2/TodoApp/app/assets/javascripts/bundle.js
@@ -28035,6 +28035,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(13);
 
 var _react2 = _interopRequireDefault(_react);
@@ -28049,30 +28051,49 @@ var _todo_form2 = _interopRequireDefault(_todo_form);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var TodoList = function TodoList(_ref) {
-  var todos = _ref.todos,
-      receiveTodo = _ref.receiveTodo,
-      removeTodo = _ref.removeTodo;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  return _react2.default.createElement(
-    "div",
-    null,
-    _react2.default.createElement(_todo_form2.default, { receiveTodo: receiveTodo }),
-    _react2.default.createElement(
-      "ul",
-      null,
-      todos.map(function (todo, id) {
-        return _react2.default.createElement(_todo_list_item2.default, { key: id + todo.title,
-          title: todo.title,
-          id: todo.id,
-          body: todo.body,
-          done: todo.done,
-          removeTodo: removeTodo,
-          receiveTodo: receiveTodo });
-      })
-    )
-  );
-};
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var TodoList = function (_React$Component) {
+  _inherits(TodoList, _React$Component);
+
+  function TodoList(props) {
+    _classCallCheck(this, TodoList);
+
+    return _possibleConstructorReturn(this, (TodoList.__proto__ || Object.getPrototypeOf(TodoList)).call(this, props));
+  }
+
+  _createClass(TodoList, [{
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      return _react2.default.createElement(
+        "div",
+        null,
+        _react2.default.createElement(_todo_form2.default, { receiveTodo: this.props.receiveTodo }),
+        _react2.default.createElement(
+          "ul",
+          null,
+          this.props.todos.map(function (todo, id) {
+            return _react2.default.createElement(_todo_list_item2.default, { key: id + todo.title,
+              title: todo.title,
+              id: todo.id,
+              body: todo.body,
+              done: todo.done,
+              removeTodo: _this2.props.removeTodo,
+              receiveTodo: _this2.props.receiveTodo });
+          })
+        )
+      );
+    }
+  }]);
+
+  return TodoList;
+}(_react2.default.Component);
 
 exports.default = TodoList;
 
--- day2/TodoApp/frontend/components/todo_list/todo_list.jsx
+++ day2/TodoApp/frontend/components/todo_list/todo_list.jsx
@@ -4,29 +4,30 @@ import TodoForm from "./todo_form";
 
 
 
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div>
+        <TodoForm receiveTodo={this.props.receiveTodo}/>
+        <ul>
+          {
+            this.props.todos.map((todo,id) => <TodoListItem key={id + todo.title}
+                                              title={todo.title}
+                                              id={todo.id}
+                                              body={todo.body}
+                                              done={todo.done}
+                                              removeTodo={this.props.removeTodo}
+                                              receiveTodo={this.props.receiveTodo}/>)
+                                            }
+        </ul>
+      </div>
+    );
+  }
+}
 
-
-
-
-
-const TodoList = ({ todos, receiveTodo, removeTodo}) => {
-  return (
-    <div>
-      <TodoForm receiveTodo={receiveTodo}/>
-      <ul>
-        {
-          todos.map((todo,id) => <TodoListItem key={id + todo.title}
-                                            title={todo.title}
-                                            id={todo.id}
-                                            body={todo.body}
-                                            done={todo.done}
-                                            removeTodo={removeTodo}
-                                            receiveTodo={receiveTodo}/>)
-                                          }
-      </ul>
-    </div>
-  );
-};
 
 export default TodoList;
